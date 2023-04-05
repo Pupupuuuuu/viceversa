9f3cf1a5cbb1a3eaac39b5e7b04eef0df63668a3
Pu_pu_puuuuu <genius101726@gmail.com>
2023-04-05 13:47:26 +0300

    Counting words

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,5 +7,8 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reverse_text = user_text[::-1]
+	count_the_number_of_words = len(user_text.split())
 	return render(request, 'reverse.html', {'usertext':user_text,
-		'reversetext':reverse_text})
+		'reversetext':reverse_text,
+		'numberwords':count_the_number_of_words
+		})
